6c85f75acfdbbf55facaac5a562919478b328bc7
zarif-webflow <zarif.webflow@gmail.com>
2026-03-15 00:50:17 +0600

    chore: update entry point

--- entypoints.mjs
+++ entypoints.mjs
@@ -2,4 +2,4 @@
  * This file defines the entry points for the application.
  * Example: ['src/target_folder/target_file.ts', 'src/target_folder2/target_file2.ts']
  */
-export const ENTRY_POINTS = ["src/expanding-carousel.ts", "src/hero-carousel.ts"];
+export const ENTRY_POINTS = ["src/expanding-carousel.ts", "src/hero-carousel.ts", "src/ms-form.ts"];
